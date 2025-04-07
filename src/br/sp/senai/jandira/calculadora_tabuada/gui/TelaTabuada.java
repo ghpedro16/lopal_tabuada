bbf370e05f67d7cef7eb47a129275affdd1f3572
PEDRO HENRIQUE ARAUJO DA SILVA <25132515@CFP127ALUNO.LOCAL>
2025-04-07 11:23:33 -0300

    criando o vetor

--- src/br/sp/senai/jandira/calculadora_tabuada/gui/TelaTabuada.java
+++ src/br/sp/senai/jandira/calculadora_tabuada/gui/TelaTabuada.java
@@ -1,51 +1,80 @@
 package br.sp.senai.jandira.calculadora_tabuada.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JTextField;
+
+import br.sp.senai.jandira.calculadora_tabuada.model.Tabuada;
 
 public class TelaTabuada {
 
+	private JLabel lblMultiplicando;
+	private JTextField txtMultiplicando;
+	private JLabel lblMinMultiplicador;
+	private JTextField txtMinMultiplicador;
+	private JLabel lblMaxMultiplicador;
+	private JTextField txtMaxMultiplicador;
+	private JButton buttonCalcular;
+	private JButton buttonLimpar;
+	private JList listTabuada;
+	private String resultado[];
+	
+	
 	public void criarTela() {
 		
 		JFrame tela = new JFrame();
-		tela.setSize(500, 700);
+		tela.setSize(265, 550);
 		tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		tela.setTitle("Calculadora de Tabuada");
 		tela.setLocationRelativeTo(null);
 		tela.setLayout(null);
+		tela.setResizable(false);
 		
 		// Criar um label para inserir no JFrame
 		// Multiplicando
-		JLabel lblMultiplicando = new JLabel();
+		lblMultiplicando = new JLabel();
 		lblMultiplicando.setText("Multiplicando:");
 		lblMultiplicando.setBounds(20, 10, 150, 30);
 		
-		JTextField txtMultiplicando = new JTextField();
+		txtMultiplicando = new JTextField();
 		txtMultiplicando.setBounds(180, 10, 50, 30);
 		
 		// Minimo Multiplicador
-		JLabel lblMinMultiplicador = new JLabel();
+		lblMinMultiplicador = new JLabel();
 		lblMinMultiplicador.setText("Mín. Multiplicador:");
 		lblMinMultiplicador.setBounds(20, 50, 150, 30);
 		
-		JTextField txtMinMultiplicador = new JTextField();
+		txtMinMultiplicador = new JTextField();
 		txtMinMultiplicador.setBounds(180, 50, 50, 30);
 		
 		// Maximo Multiplicador
-		JLabel lblMaxMultiplicador = new JLabel();
+		lblMaxMultiplicador = new JLabel();
 		lblMaxMultiplicador.setText("Máx. Multiplicador:");
 		lblMaxMultiplicador.setBounds(20, 90, 150, 30);
 		
-		JTextField txtMaxMultiplicador = new JTextField();
+	    txtMaxMultiplicador = new JTextField();
 		txtMaxMultiplicador.setBounds(180, 90, 50, 30);
 		
-		
-		
-		JButton buttonCalcular = new JButton();
+		// Botão calcular
+		buttonCalcular = new JButton();
 		buttonCalcular.setText("Calcular");
+		buttonCalcular.setBounds(20, 140, 100, 30);
 		
+		// Botão limpar
+		buttonLimpar = new JButton();
+		buttonLimpar.setText("Limpar");
+		buttonLimpar.setBounds(130, 140, 100, 30);
+		
+		// Lista pra exibir tabuada
+		listTabuada = new JList(resultado);
+		listTabuada.setBounds(20, 190, 210, 300);
+		
+		// Adicionar componentes na tela
 		tela.getContentPane().add(lblMultiplicando);
 		tela.getContentPane().add(txtMultiplicando);
 		tela.getContentPane().add(lblMinMultiplicador);
@@ -53,10 +82,38 @@ public class TelaTabuada {
 		tela.getContentPane().add(lblMaxMultiplicador);
 		tela.getContentPane().add(txtMaxMultiplicador);
 		
-		
 		tela.getContentPane().add(buttonCalcular);
+		tela.getContentPane().add(buttonLimpar);
+		tela.getContentPane().add(listTabuada);
 		
 		
+		// Configurar os ouvintes (listeners) dos botões
+		buttonCalcular.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				String multiplicando = txtMultiplicando.getText();
+				String minMultiplicador = txtMinMultiplicador.getText();
+				String maxMultiplicador = txtMaxMultiplicador.getText();
+				
+				// Casting -> conversão de um tipo para outro
+				double multiplicandoDouble = Double.parseDouble(multiplicando);
+				double minMultiplicadorDouble = Double.parseDouble(minMultiplicador);
+				double maxMultiplicadorDouble = Double.parseDouble(maxMultiplicador);
+				
+				Tabuada tabuada = new Tabuada();
+				tabuada.setMultiplicando(multiplicandoDouble);
+				tabuada.setMinimoMultiplicador(minMultiplicadorDouble);
+				tabuada.setMaximoMultiplicador(maxMultiplicadorDouble);
+				resultado = tabuada.calcularTabuada();
+				
+			
+			}
+		});
+		
+		
+		// Tornar a tela visível
 		tela.setVisible(true);
 		
 	}
--- src/br/sp/senai/jandira/calculadora_tabuada/model/Tabuada.java
+++ src/br/sp/senai/jandira/calculadora_tabuada/model/Tabuada.java
@@ -32,7 +32,7 @@ public class Tabuada {
 	}
 
 	
-	public void calcularTabuada() {
+	public String[] calcularTabuada() {
 		// Verificar se os valores estão invertidos
 		if(minimoMultiplicador > maximoMultiplicador) {
 			double temp = minimoMultiplicador;
@@ -40,11 +40,22 @@ public class Tabuada {
 			maximoMultiplicador = temp;
 		}
 		
+		// Criar vetor de String que armazenará cada linha da tabuada
+		int tamanho = (int) (maximoMultiplicador - minimoMultiplicador + 1);
+		String resultado[] = new String[tamanho];
+		
+		// Calcular e mostrar tabuada
+		int contador = 0;
 		while (minimoMultiplicador <= maximoMultiplicador){
             double produto = multiplicando * minimoMultiplicador;
-            System.out.printf("%s X %s = %s\n", multiplicando, minimoMultiplicador, produto);
+            String linha = multiplicando + " X " + minimoMultiplicador + " = " + produto;
+            resultado[contador] = linha;
+            System.out.println(linha);
             minimoMultiplicador++;
+            contador ++;
         }
+		
+		return resultado;
 		
 	}
 	
